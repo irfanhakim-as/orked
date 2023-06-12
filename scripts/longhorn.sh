3f11c364e5cda731a2c631a8c3346236cb931d22
irfanhakim-as <irfanhakim.as@yahoo.com>
2023-06-12 09:15:53 +0800

    Update longhorn installation

--- scripts/longhorn.sh
+++ scripts/longhorn.sh
@@ -28,10 +28,58 @@ for ((i = 0; i < ${#worker_hostnames[@]}; i++)); do
     # mount dedicated data storage
     mount /dev/sdb /var/lib/longhorn
 
-    # start and enable RKE2 agent service
-    systemctl enable --now rke2-agent.service
+    # add to fstab
+    echo "/dev/sdb                /var/lib/longhorn       ext4    defaults        0 0" >> /etc/fstab
 EOF
+done
+
+# install open-iscsi
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.4.1/deploy/prerequisite/longhorn-iscsi-installation.yaml
+
+# wait for longhorn-iscsi-installation to be ready
+while true; do
+  longhorn_iscsi_pod=$(kubectl get pods -n longhorn-system | grep 'Running' | grep 'longhorn-iscsi-installation' | awk '{print $1}')
+  if [ -z "${longhorn_iscsi_pod}" ]; then
+    echo "Waiting for longhorn-iscsi-installation to be ready..."
+    sleep 5
+  else
+    break
+  fi
+done
+
+# install NFSv4 client
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.4.1/deploy/prerequisite/longhorn-nfs-installation.yaml
+
+# wait for longhorn-nfs-installation to be ready
+while true; do
+  longhorn_nfs_pod=$(kubectl get pods -n longhorn-system | grep 'Running' | grep 'longhorn-nfs-installation' | awk '{print $1}')
+  if [ -z "${longhorn_nfs_pod}" ]; then
+    echo "Waiting for longhorn-nfs-installation to be ready..."
+    sleep 5
+  else
+    break
+  fi
 done
 
 # install jq
 sudo yum install -y jq
+
+# ensure nodes have all the necessary tools to install longhorn
+curl -sSfL https://raw.githubusercontent.com/longhorn/longhorn/v1.4.1/scripts/environment_check.sh | bash
+
+# install longhorn
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.4.1/deploy/longhorn.yaml
+
+# wait for longhorn to be ready
+while true; do
+  longhorn_pod=$(kubectl get pods -n longhorn-system | grep 'Running' | grep 'longhorn-manager' | awk '{print $1}')
+  if [ -z "${longhorn_pod}" ]; then
+    echo "Waiting for longhorn to be ready..."
+    sleep 5
+  else
+    break
+  fi
+done
+
+# check storage class
+kubectl get sc
