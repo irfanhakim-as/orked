a9293e1f5ad8823e9a168b842e167b1aea0488ba
irfanhakim-as <irfanhakim.as@yahoo.com>
2023-06-13 10:08:31 +0800

    Support kubernetes node configuration with svc account

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
-# configure networking
-interface="[keyfile]\nunmanaged-devices=interface-name:cali*;interface-name:flannel*"
-echo -e "${interface}" | sudo tee "/etc/NetworkManager/conf.d/rke2-canal.conf" > /dev/null
+# get service user account
+service_user=$(bash ./utils.sh --get-data "service user account")
 
-# disable additional services in rocky linux 8
-sudo systemctl disable nm-cloud-setup.service && sudo systemctl disable nm-cloud-setup.timer
+# get sudo password
+echo "Enter sudo password:"
+sudo_password=$(bash ./utils.sh --get-password)
 
-# stop and disable firewalld
-sudo systemctl disable --now firewalld
+# get hostnames of all kubernetes nodes
+kubernetes_hostnames=($(bash ./utils.sh --get-values "hostname of kubernetes node"))
 
-# disable swap
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab && sudo swapoff -a
+# configure each node
+for ((i = 0; i < ${#kubernetes_hostnames[@]}; i++)); do
+    kubernetes_hostname="${kubernetes_hostnames[${i}]}"
+    echo "Configuring node: ${kubernetes_hostname}"
 
-# modify bridge adapter settings
-sudo modprobe br_netfilter
-bridge="net.bridge.bridge-nf-call-ip6tables = 1\nnet.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.ip_forward = 1\nnet.ipv6.conf.all.forwarding = 1"
-echo -e "${bridge}" | sudo tee "/etc/sysctl.d/kubernetes.conf" > /dev/null
+    # remote login into kubernetes node
+    ssh "${service_user}@${kubernetes_hostname}" 'bash -s' << EOF
+        # configure networking
+        interface="[keyfile]\nunmanaged-devices=interface-name:cali*;interface-name:flannel*"
+        command="echo -e '\${interface}' | sudo tee '/etc/NetworkManager/conf.d/rke2-canal.conf' > /dev/null"
+        echo ${sudo_password} | sudo -S bash -c "\${command}"
 
-# reboot
-sudo reboot now
+        # disable additional services in rocky linux 8
+        echo ${sudo_password} | sudo -S bash -c "sudo systemctl disable nm-cloud-setup.service && sudo systemctl disable nm-cloud-setup.timer"
+
+        # stop and disable firewalld
+        echo ${sudo_password} | sudo -S bash -c "sudo systemctl disable --now firewalld"
+
+        # disable swap
+        echo ${sudo_password} | sudo -S bash -c "sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab && sudo swapoff -a"
+
+        # modify bridge adapter settings
+        bridge="net.bridge.bridge-nf-call-ip6tables = 1\nnet.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.ip_forward = 1\nnet.ipv6.conf.all.forwarding = 1"
+        command="echo -e '\${bridge}' | sudo tee '/etc/sysctl.d/kubernetes.conf' > /dev/null"
+        echo ${sudo_password} | sudo -S bash -c "\${command}"
+
+        # reboot
+        echo ${sudo_password} | sudo -S bash -c "sudo reboot now"
+EOF
+done
