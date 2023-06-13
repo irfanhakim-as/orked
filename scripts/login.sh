6a41f4f9bc4262078c5bfb9624931cb12decf838
irfanhakim-as <irfanhakim.as@yahoo.com>
2023-06-13 10:40:25 +0800

    Get sudo password a single time at the start of the script

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+# get sudo password
+echo "Enter sudo password:"
+sudo_password=$(bash ./utils.sh --get-password)
+
 # install and enable docker
 if bash ./utils.sh --is-installed docker; then
     echo "Docker is already installed"
 else
     curl https://releases.rancher.com/install-docker/20.10.sh | sh \
-    && sudo usermod -aG docker ${USER} \
-    && sudo systemctl enable --now docker
+    && echo ${sudo_password} | sudo -S bash -c "usermod -aG docker ${USER}" \
+    && echo ${sudo_password} | sudo -S bash -c "systemctl enable --now docker"
 fi
 
 # install git
 if bash ./utils.sh --is-installed git; then
     echo "Git is already installed"
 else
-    sudo yum install -y git
+    echo ${sudo_password} | sudo -S bash -c "yum install -y git"
 fi
 
 # install kubectl
@@ -22,7 +26,7 @@ if bash ./utils.sh --is-installed kubectl; then
 else
     curl -Lo kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
     && chmod +x kubectl \
-    && sudo mv kubectl /usr/local/bin
+    && echo ${sudo_password} | sudo -S bash -c "mv kubectl /usr/local/bin"
 fi
 
 # if both kubectx and kubens are installed, skip
@@ -30,9 +34,9 @@ if bash ./utils.sh --is-installed kubectx && bash ./utils.sh --is-installed kube
     echo "Kubectx and Kubens are already installed"
 else
     # install kubectx and kubens
-    sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx \
-    && sudo ln -sf /opt/kubectx/kubectx /usr/local/bin/kubectx \
-    && sudo ln -sf /opt/kubectx/kubens /usr/local/bin/kubens
+    echo ${sudo_password} | sudo -S bash -c "git clone https://github.com/ahmetb/kubectx /opt/kubectx" \
+    && echo ${sudo_password} | sudo -S bash -c "ln -sf /opt/kubectx/kubectx /usr/local/bin/kubectx" \
+    && echo ${sudo_password} | sudo -S bash -c "ln -sf /opt/kubectx/kubens /usr/local/bin/kubens"
 fi
 
 # install k9s
@@ -40,12 +44,13 @@ if bash ./utils.sh --is-installed k9s; then
     echo "K9s is already installed"
 else
     curl -Lo k9s_Linux_x86_64.tar.gz "https://github.com/derailed/k9s/releases/download/v0.27.4/k9s_Linux_amd64.tar.gz" \
-    && sudo tar -C /usr/local/bin -zxf k9s_Linux_x86_64.tar.gz k9s
+    && echo ${sudo_password} | sudo -S bash -c "tar -C /usr/local/bin -zxf k9s_Linux_x86_64.tar.gz k9s" \
+    && rm -f k9s_Linux_x86_64.tar.gz
 fi
 
 # install helm
 if bash ./utils.sh --is-installed helm; then
     echo "Helm is already installed"
 else
-    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    echo ${sudo_password} | sudo -S bash -c "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
 fi
