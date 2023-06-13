efffe94a8fe01daac6a1f0f11607aa109359c4b1
irfanhakim-as <irfanhakim.as@yahoo.com>
2023-06-13 10:17:07 +0800

    Remove redundant sudo

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -19,24 +19,24 @@ for ((i = 0; i < ${#kubernetes_hostnames[@]}; i++)); do
     ssh "${service_user}@${kubernetes_hostname}" 'bash -s' << EOF
         # configure networking
         interface="[keyfile]\nunmanaged-devices=interface-name:cali*;interface-name:flannel*"
-        command="echo -e '\${interface}' | sudo tee '/etc/NetworkManager/conf.d/rke2-canal.conf' > /dev/null"
+        command="echo -e '\${interface}' | tee '/etc/NetworkManager/conf.d/rke2-canal.conf' > /dev/null"
         echo ${sudo_password} | sudo -S bash -c "\${command}"
 
         # disable additional services in rocky linux 8
-        echo ${sudo_password} | sudo -S bash -c "sudo systemctl disable nm-cloud-setup.service && sudo systemctl disable nm-cloud-setup.timer"
+        echo ${sudo_password} | sudo -S bash -c "systemctl disable nm-cloud-setup.service && systemctl disable nm-cloud-setup.timer"
 
         # stop and disable firewalld
-        echo ${sudo_password} | sudo -S bash -c "sudo systemctl disable --now firewalld"
+        echo ${sudo_password} | sudo -S bash -c "systemctl disable --now firewalld"
 
         # disable swap
-        echo ${sudo_password} | sudo -S bash -c "sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab && sudo swapoff -a"
+        echo ${sudo_password} | sudo -S bash -c "sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab && swapoff -a"
 
         # modify bridge adapter settings
         bridge="net.bridge.bridge-nf-call-ip6tables = 1\nnet.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.ip_forward = 1\nnet.ipv6.conf.all.forwarding = 1"
-        command="echo -e '\${bridge}' | sudo tee '/etc/sysctl.d/kubernetes.conf' > /dev/null"
+        command="echo -e '\${bridge}' | tee '/etc/sysctl.d/kubernetes.conf' > /dev/null"
         echo ${sudo_password} | sudo -S bash -c "\${command}"
 
         # reboot
-        echo ${sudo_password} | sudo -S bash -c "sudo reboot now"
+        echo ${sudo_password} | sudo -S bash -c "reboot now"
 EOF
 done
