4d3db2d3eefbd9fb395e4397b329e812ca98f541
irfanhakim-as <irfanhakim.as@yahoo.com>
2024-02-16 16:35:54 +0800

    Add script to install df-pv

--- scripts/login.sh
+++ scripts/login.sh
@@ -65,5 +65,15 @@ else
     rm -f "${HOME}/pv-migrate.tar.gz"
 fi
 
+# install df-pv
+if [ "$(bash ./utils.sh --is-installed df-pv)" = "true" ]; then
+    echo "df-pv is already installed"
+else
+    dfpvVersion=$(curl -s "https://api.github.com/repos/yashbhutwala/kubectl-df-pv/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+    curl -Lo "${HOME}/df-pv.tar.gz" "https://github.com/yashbhutwala/kubectl-df-pv/releases/download/v${dfpvVersion}/kubectl-df-pv_v${dfpvVersion}_linux_amd64.tar.gz"
+    echo ${sudo_password} | sudo -S bash -c "tar -C "/usr/local/bin" -xzf "${HOME}/df-pv.tar.gz" df-pv"
+    rm -f "${HOME}/df-pv.tar.gz"
+fi
+
 # reboot
 echo ${sudo_password} | sudo -S bash -c "reboot now"
