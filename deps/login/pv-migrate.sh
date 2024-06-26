0b3d0a978e4e1ee0cb86f172b98fbaf106e25f57
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-06-27 00:55:15 +0800

    Fix bug where latest version is installed regardless

--- deps/login/pv-migrate.sh
+++ deps/login/pv-migrate.sh
@@ -16,10 +16,11 @@ SYS_PLATFORM="${SYS_PLATFORM:-"linux"}"
 INSTALL_PFX="${INSTALL_PFX:-"/usr/local"}"
 PKG_BIN="${PKG_BIN:-"$(basename "${PKG_BIN_SRC}")"}"
 PKG_API_URL="https://api.github.com/repos/${PKG_REPO}/releases/latest"
-PKG_DL_URL="https://github.com/${PKG_REPO}/releases/latest/download"
+PKG_DL_URL="https://github.com/${PKG_REPO}/releases/download"
 PKG_LATEST_VER=$(curl -s "${PKG_API_URL}" | perl -lne 'print $& if /"tag_name": "v\K[0-9.]+/')
 PKG_SRC_VER="${PKG_SRC_VER:-"${PKG_LATEST_VER}"}"
 PKG_SRC_ARCHIVE="${PKG_NAME}_v${PKG_SRC_VER}_${SYS_PLATFORM}_${SYS_ARCH}.${PKG_ARCHIVE_FMT}"
+PKG_SRC_URL="${PKG_DL_URL}/${PKG_SRC_VER}/${PKG_SRC_ARCHIVE}"
 PKG_TMP_ARCHIVE="/tmp/${PKG_SRC_ARCHIVE}"
 PKG_TMP_DIR=$(dirname "${PKG_TMP_ARCHIVE}")
 PKG_INSTALL_DIR="${INSTALL_PFX}/bin"
@@ -46,7 +47,7 @@ if [ "$(bash "${SCRIPT_PATH}/utils.sh" --is-installed ${PKG_BIN})" = "false" ]; 
     # create target directories
     bash "${SCRIPT_PATH}/utils.sh" --sudo-if-needed mkdir -p "${PKG_TMP_DIR}" "${PKG_INSTALL_DIR}"
     # download package from source
-    bash "${SCRIPT_PATH}/utils.sh" --sudo-if-needed curl -fLo "${PKG_TMP_ARCHIVE}" "${PKG_DL_URL}/${PKG_SRC_ARCHIVE}" || { echo "ERROR: failed to download package"; exit 1; }
+    bash "${SCRIPT_PATH}/utils.sh" --sudo-if-needed curl -fLo "${PKG_TMP_ARCHIVE}" "${PKG_SRC_URL}" || { echo "ERROR: failed to download package"; exit 1; }
     # unpack package to installation directory
     if [[ "${PKG_TMP_ARCHIVE}" == *.tar.gz ]]; then
         # requires tar
