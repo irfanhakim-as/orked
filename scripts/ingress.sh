eca2b3ac349f152ad4aa48ab916df973476f979e
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-06-26 13:27:41 +0800

    Rename manifest file

--- scripts/ingress.sh
+++ scripts/ingress.sh
@@ -6,15 +6,15 @@ SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # dependency path
 DEP_PATH="${SOURCE_DIR}/../deps"
 
-# download nginx-ingress manifest
+# copy ingress-nginx manifest
 # source: https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.1/deploy/static/provider/baremetal/deploy.yaml
-cp -f "${DEP_PATH}/ingress/deploy.yaml" ~/nginx-ingress.yaml
+cp -f "${DEP_PATH}/ingress/deploy.yaml" ~/ingress-nginx.yaml
 
 # replace NodePort with LoadBalancer
-sed -i 's/type: NodePort/type: LoadBalancer/g' ~/nginx-ingress.yaml
+sed -i 's/type: NodePort/type: LoadBalancer/g' ~/ingress-nginx.yaml
 
 # install nginx ingress
-kubectl apply -f ~/nginx-ingress.yaml
+kubectl apply -f ~/ingress-nginx.yaml
 
 # wait until no pods are pending
 bash "${SOURCE_DIR}/utils.sh" --wait-for-pods ingress-nginx
