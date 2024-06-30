1802923c04fad01dff35f5fc30ad1608602e1c1c
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-06-30 20:16:55 +0800

    Refactor paths in installer script

--- scripts/login/utils/k9s.sh
+++ scripts/login/utils/k9s.sh
@@ -2,7 +2,7 @@
 
 # get script source
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-SCRIPT_PATH="${SOURCE_DIR}/../../scripts"
+SCRIPT_PATH="${SOURCE_DIR}/../.."
 
 # variables
 PKG_NAME="${PKG_NAME:-"k9s"}"
--- scripts/login/utils/yum.sh
+++ scripts/login/utils/yum.sh
@@ -2,9 +2,8 @@
 
 # get script source
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-# script path
-SCRIPT_PATH="${SOURCE_DIR}/../../scripts"
+SCRIPT_PATH="${SOURCE_DIR}/../.."
+DEP_PATH="${SOURCE_DIR}/../../../deps"
 
 # update yum repo
 bash "${SCRIPT_PATH}/utils.sh" --sudo yum update -y
@@ -13,7 +12,7 @@ bash "${SCRIPT_PATH}/utils.sh" --sudo yum update -y
 bash "${SCRIPT_PATH}/utils.sh" --sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 
 # install dependencies
-xargs bash "${SCRIPT_PATH}/utils.sh" --sudo yum install -y < "${SOURCE_DIR}/yum.txt"
+xargs bash "${SCRIPT_PATH}/utils.sh" --sudo yum install -y < "${DEP_PATH}/login/yum.txt"
 
 # clean up cache and unused dependencies
 bash "${SCRIPT_PATH}/utils.sh" --sudo yum clean all -y && bash "${SCRIPT_PATH}/utils.sh" --sudo yum autoremove -y
