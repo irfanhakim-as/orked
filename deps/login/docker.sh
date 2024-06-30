ab9e929d83d16e79419cbd5d9eabbe203655a2d9
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-06-30 19:50:08 +0800

    Add docker installer script

---
+++ deps/login/docker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# get script source
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+SCRIPT_PATH="${SOURCE_DIR}/../../scripts"
+
+# variables
+PKG_NAME="docker"
+
+if [ "$(bash "${SCRIPT_PATH}/utils.sh" --is-installed ${PKG_NAME})" = "false" ]; then
+    echo "Installing ${PKG_NAME}..."
+    # run installer
+    bash "${SCRIPT_PATH}/utils.sh" --sudo "curl https://releases.rancher.com/install-docker/20.10.sh | sh"
+    # create group docker
+    bash "${SCRIPT_PATH}/utils.sh" --sudo groupadd docker
+    # add current user to docker group
+    bash "${SCRIPT_PATH}/utils.sh" --sudo usermod -aG docker "${USER}"
+    # enable docker service
+    bash "${SCRIPT_PATH}/utils.sh" --sudo systemctl enable --now docker
+else
+    echo "${PKG_NAME} is already installed"
+fi
