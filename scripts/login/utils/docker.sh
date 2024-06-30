9ec4191741ea8d6b8c133e0738aee07970354f5d
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-06-30 20:29:52 +0800

    Source script locally

--- scripts/login/utils/docker.sh
+++ scripts/login/utils/docker.sh
@@ -3,6 +3,7 @@
 # get script source
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 SCRIPT_PATH="${SOURCE_DIR}/../.."
+DEP_PATH="${SOURCE_DIR}/../../../deps"
 
 # variables
 PKG_NAME="docker"
@@ -10,7 +11,8 @@ PKG_NAME="docker"
 if [ "$(bash "${SCRIPT_PATH}/utils.sh" --is-installed ${PKG_NAME})" = "false" ]; then
     echo "Installing ${PKG_NAME}..."
     # run installer
-    bash "${SCRIPT_PATH}/utils.sh" --sudo "curl https://releases.rancher.com/install-docker/20.10.sh | sh"
+    # source: https://releases.rancher.com/install-docker/20.10.sh
+    bash "${SCRIPT_PATH}/utils.sh" --sudo sh "${DEP_PATH}/login/20.10.sh"
     # create group docker
     bash "${SCRIPT_PATH}/utils.sh" --sudo groupadd docker
     # add current user to docker group
