fdd921914f1a97eea84283ead91b23c14627bea6
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-06-30 20:21:15 +0800

    Refactor paths in login setup script

--- scripts/login/login.sh
+++ scripts/login/login.sh
@@ -2,38 +2,40 @@
 
 # get script source
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+SCRIPT_PATH="${SOURCE_DIR}/.."
+UTILS_PATH="${SOURCE_DIR}/utils"
 
 # get sudo password
 echo "Enter sudo password:"
-export sudo_password=$(bash "${SOURCE_DIR}/utils.sh" --get-password)
+export sudo_password=$(bash "${SCRIPT_PATH}/utils.sh" --get-password)
 
 # setup yum repo and dependencies
-bash "${SOURCE_DIR}/yum.sh"
+bash "${UTILS_PATH}/yum.sh"
 
 # install docker
-bash "${SOURCE_DIR}/docker.sh"
+bash "${UTILS_PATH}/docker.sh"
 
 # install kubectl
-bash "${SOURCE_DIR}/kubectl.sh"
+bash "${UTILS_PATH}/kubectl.sh"
 
 # install kubectx and kubens
-bash "${SOURCE_DIR}/kubectx.sh" && PKG_NAME="kubens" bash "${SOURCE_DIR}/kubectx.sh"
+bash "${UTILS_PATH}/kubectx.sh" && PKG_NAME="kubens" bash "${UTILS_PATH}/kubectx.sh"
 
 # install k9s
-PKG_SRC_VER="0.26.4" bash "${SOURCE_DIR}/k9s.sh"
+PKG_SRC_VER="0.26.4" bash "${UTILS_PATH}/k9s.sh"
 
 # install helm
-if [ "$(bash "${SOURCE_DIR}/utils.sh" --is-installed helm)" = "true" ]; then
+if [ "$(bash "${SCRIPT_PATH}/utils.sh" --is-installed helm)" = "true" ]; then
     echo "Helm is already installed"
 else
     echo ${sudo_password} | sudo -S bash -c "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
 fi
 
 # install pv-migrate
-bash "${SOURCE_DIR}/pv-migrate.sh"
+bash "${UTILS_PATH}/pv-migrate.sh"
 
 # install df-pv
-bash "${SOURCE_DIR}/df-pv.sh"
+bash "${UTILS_PATH}/df-pv.sh"
 
 # reboot
 bash "${SCRIPT_PATH}/utils.sh" --sudo reboot now
