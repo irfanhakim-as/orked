4ab4496afb08766552bd48dd8dc75663d69df73a
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-07-03 04:19:53 +0800

    Source utils

--- scripts/login/utils/helm.sh
+++ scripts/login/utils/helm.sh
@@ -5,14 +5,20 @@ SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 SCRIPT_PATH="${SOURCE_DIR}/../.."
 DEP_PATH="${SOURCE_DIR}/../../../deps"
 
+# source project files
+source "${SCRIPT_PATH}/utils.sh"
+
 # variables
 PKG_NAME="helm"
 
-if [ "$(bash "${SCRIPT_PATH}/utils.sh" --is-installed ${PKG_NAME})" = "false" ]; then
+
+# ================= DO NOT EDIT BEYOND THIS LINE =================
+
+if [ "$(is_installed "${PKG_NAME}")" = "false" ]; then
     echo "Installing ${PKG_NAME}..."
     # run installer
     # source: https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-    bash "${SCRIPT_PATH}/utils.sh" --sudo bash "${DEP_PATH}/login/helm.sh"
+    run_with_sudo bash "${DEP_PATH}/login/helm.sh"
 else
     echo "${PKG_NAME} is already installed"
 fi
