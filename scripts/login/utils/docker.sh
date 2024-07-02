4ed56b7091232a1cf1ceaf3ffeb3861409f288af
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-07-03 04:18:24 +0800

    Source utils

--- scripts/login/utils/docker.sh
+++ scripts/login/utils/docker.sh
@@ -5,20 +5,26 @@ SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 SCRIPT_PATH="${SOURCE_DIR}/../.."
 DEP_PATH="${SOURCE_DIR}/../../../deps"
 
+# source project files
+source "${SCRIPT_PATH}/utils.sh"
+
 # variables
 PKG_NAME="docker"
 
-if [ "$(bash "${SCRIPT_PATH}/utils.sh" --is-installed ${PKG_NAME})" = "false" ]; then
+
+# ================= DO NOT EDIT BEYOND THIS LINE =================
+
+if [ "$(is_installed "${PKG_NAME}")" = "false" ]; then
     echo "Installing ${PKG_NAME}..."
     # run installer
     # source: https://releases.rancher.com/install-docker/20.10.sh
-    bash "${SCRIPT_PATH}/utils.sh" --sudo sh "${DEP_PATH}/login/20.10.sh"
+    run_with_sudo sh "${DEP_PATH}/login/20.10.sh"
     # create group docker
-    bash "${SCRIPT_PATH}/utils.sh" --sudo groupadd docker
+    run_with_sudo groupadd docker
     # add current user to docker group
-    bash "${SCRIPT_PATH}/utils.sh" --sudo usermod -aG docker "${USER}"
+    run_with_sudo usermod -aG docker "${USER}"
     # enable docker service
-    bash "${SCRIPT_PATH}/utils.sh" --sudo systemctl enable --now docker
+    run_with_sudo systemctl enable --now docker
 else
     echo "${PKG_NAME} is already installed"
 fi
