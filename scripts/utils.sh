ead7e6a832a846235e71170b87409d6458df466b
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-07-03 02:16:33 +0800

    Comment args

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -122,92 +122,92 @@ function print_help() {
 }
 
 # get arguments on what function to run
-while [[ $# -gt 0 ]]; do
-    case "${1}" in
-        --file-ends-with-newline)
-            if [ -z "${2}" ]; then
-                echo "Please provide the file you are checking!"
-                exit 1
-            fi
-            file_ends_with_newline "${2}"
-            shift
-            ;;
-        --get-data)
-            if [ -z "${2}" ]; then
-                echo "Please provide information of the data you are requesting!"
-                exit 1
-            fi
-            get_data "${2}"
-            shift
-            ;;
-        --get-password)
-            get_password
-            shift
-            ;;
-        --get-secret)
-            if [ -z "${2}" ]; then
-                echo "Please provide information of the secret you are requesting!"
-                exit 1
-            fi
-            get_secret "${2}"
-            shift
-            ;;
-        --get-values)
-            if [ -z "${2}" ]; then
-                echo "Please provide information of what you are requesting!"
-                exit 1
-            fi
-            get_values "${2}"
-            shift
-            ;;
-        --is-installed)
-            if [ -z "${2}" ]; then
-                echo "Please provide the command(s) you wish to check are installed!"
-                exit 1
-            fi
-            is_installed "${@:2}"
-            shift
-            ;;
-        --sudo)
-            if [ -z "${2}" ]; then
-                echo "Please provide the command(s) you wish to run with sudo!"
-                exit 1
-            fi
-            run_with_sudo "${@:2}"
-            shift
-            ;;
-        --sudo-if-needed)
-            if [ -z "${2}" ]; then
-                echo "Please provide the command(s) you wish to run!"
-                exit 1
-            fi
-            sudo_if_needed "${@:2}"
-            shift
-            ;;
-        --update-config)
-            if [ -z "${2}" ] || [ -z "${3}" ] || [ -z "${4}" ]; then
-                echo "Please provide the file, key, and value you wish to update!"
-                exit 1
-            fi
-            update_config "${2}" "${3}" "${4}"
-            shift 3
-            ;;
-        --wait-for-pods)
-            if [ -z "${2}" ]; then
-                echo "Please provide a namespace!"
-                exit 1
-            fi
-            wait_for_pods "${2}" "${3}"
-            shift 2
-            ;;
-        -h|--help)
-            print_help
-            shift
-            ;;
-        # *)
-        #     echo "Invalid argument: ${1}"
-        #     exit 1
-        #     ;;
-    esac
-    shift
-done
+# while [[ $# -gt 0 ]]; do
+#     case "${1}" in
+#         --file-ends-with-newline)
+#             if [ -z "${2}" ]; then
+#                 echo "Please provide the file you are checking!"
+#                 exit 1
+#             fi
+#             file_ends_with_newline "${2}"
+#             shift
+#             ;;
+#         --get-data)
+#             if [ -z "${2}" ]; then
+#                 echo "Please provide information of the data you are requesting!"
+#                 exit 1
+#             fi
+#             get_data "${2}"
+#             shift
+#             ;;
+#         --get-password)
+#             get_password
+#             shift
+#             ;;
+#         --get-secret)
+#             if [ -z "${2}" ]; then
+#                 echo "Please provide information of the secret you are requesting!"
+#                 exit 1
+#             fi
+#             get_secret "${2}"
+#             shift
+#             ;;
+#         --get-values)
+#             if [ -z "${2}" ]; then
+#                 echo "Please provide information of what you are requesting!"
+#                 exit 1
+#             fi
+#             get_values "${2}"
+#             shift
+#             ;;
+#         --is-installed)
+#             if [ -z "${2}" ]; then
+#                 echo "Please provide the command(s) you wish to check are installed!"
+#                 exit 1
+#             fi
+#             is_installed "${@:2}"
+#             shift
+#             ;;
+#         --sudo)
+#             if [ -z "${2}" ]; then
+#                 echo "Please provide the command(s) you wish to run with sudo!"
+#                 exit 1
+#             fi
+#             run_with_sudo "${@:2}"
+#             shift
+#             ;;
+#         --sudo-if-needed)
+#             if [ -z "${2}" ]; then
+#                 echo "Please provide the command(s) you wish to run!"
+#                 exit 1
+#             fi
+#             sudo_if_needed "${@:2}"
+#             shift
+#             ;;
+#         --update-config)
+#             if [ -z "${2}" ] || [ -z "${3}" ] || [ -z "${4}" ]; then
+#                 echo "Please provide the file, key, and value you wish to update!"
+#                 exit 1
+#             fi
+#             update_config "${2}" "${3}" "${4}"
+#             shift 3
+#             ;;
+#         --wait-for-pods)
+#             if [ -z "${2}" ]; then
+#                 echo "Please provide a namespace!"
+#                 exit 1
+#             fi
+#             wait_for_pods "${2}" "${3}"
+#             shift 2
+#             ;;
+#         -h|--help)
+#             print_help
+#             shift
+#             ;;
+#         # *)
+#         #     echo "Invalid argument: ${1}"
+#         #     exit 1
+#         #     ;;
+#     esac
+#     shift
+# done
