1dbff53e14f018cc5ac08a7bfa9a8aab28033155
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-07-03 13:02:25 +0800

    Source utils

--- helpers/selinux-toggle.sh
+++ helpers/selinux-toggle.sh
@@ -2,19 +2,23 @@
 
 # get script source
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-# script path
 SCRIPT_PATH="${SOURCE_DIR}/../scripts"
 
+# source project files
+source "${SCRIPT_PATH}/utils.sh"
+
+
+# ================= DO NOT EDIT BEYOND THIS LINE =================
+
 # get service user account
-service_user=$(bash "${SCRIPT_PATH}/utils.sh" --get-data "service user account")
+service_user=$(get_data "service user account")
 
 # get sudo password
 echo "Enter sudo password:"
-sudo_password=$(bash "${SCRIPT_PATH}/utils.sh" --get-password)
+sudo_password=$(get_password)
 
 # get all hostnames of worker nodes
-worker_hostnames=($(bash "${SCRIPT_PATH}/utils.sh" --get-values "hostname of worker node"))
+worker_hostnames=($(get_values "hostname of worker node"))
 
 # toggle SELinux for each worker node
 for ((i = 0; i < ${#worker_hostnames[@]}; i++)); do
