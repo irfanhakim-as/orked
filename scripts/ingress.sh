0ae2b96b49c4f68b31c853e695a01307c3b93cd3
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-07-03 13:25:25 +0800

    Source utils

--- scripts/ingress.sh
+++ scripts/ingress.sh
@@ -2,9 +2,13 @@
 
 # get script source
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-# dependency path
 DEP_PATH="${SOURCE_DIR}/../deps"
+
+# source project files
+source "${SOURCE_DIR}/utils.sh"
+
+
+# ================= DO NOT EDIT BEYOND THIS LINE =================
 
 # copy ingress-nginx manifest
 # source: https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.1/deploy/static/provider/baremetal/deploy.yaml
@@ -17,7 +21,7 @@ sed -i 's/type: NodePort/type: LoadBalancer/g' ~/ingress-nginx.yaml
 kubectl apply -f ~/ingress-nginx.yaml
 
 # wait until no pods are pending
-bash "${SOURCE_DIR}/utils.sh" --wait-for-pods ingress-nginx
+wait_for_pods ingress-nginx
 
 # get ingress-nginx-controller service
 kubectl get svc ingress-nginx-controller -n ingress-nginx
