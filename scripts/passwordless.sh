86b72519f556a7e1f6269fdac485ec96ab8fc458
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-07-04 16:47:33 +0800

    Make ssh port configurable

--- scripts/passwordless.sh
+++ scripts/passwordless.sh
@@ -6,6 +6,9 @@ SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # source project files
 source "${SOURCE_DIR}/utils.sh"
 
+# variables
+port="${port:-"22"}"
+
 
 # ================= DO NOT EDIT BEYOND THIS LINE =================
 
@@ -27,5 +30,5 @@ fi
 echo "Nodes:"
 for kubernetes_hostname in "${kubernetes_hostnames[@]}"; do
     echo "Copying public SSH key to ${service_user}@${kubernetes_hostname}"
-    ssh-copy-id -i ~/.ssh/id_ecdsa.pub "${service_user}@${kubernetes_hostname}"
+    ssh-copy-id -i ~/.ssh/id_ecdsa.pub -p "${port}" "${service_user}@${kubernetes_hostname}"
 done
