90a9b353a20c89317f7baadc8921892452ebf0a3
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-07-04 22:23:22 +0800

    Make vars configurable

--- helpers/update-connection.sh
+++ helpers/update-connection.sh
@@ -10,20 +10,18 @@ source "${SCRIPT_PATH}/utils.sh"
 # variables
 IFCFG_INTERFACE="${IFCFG_INTERFACE:-"ens192"}"
 IFCFG_CONFIG="${IFCFG_CONFIG:-"/etc/sysconfig/network-scripts/ifcfg-${IFCFG_INTERFACE}"}"
+IFCFG_BOOTPROTO="${IFCFG_BOOTPROTO:-"none"}"
+IFCFG_IPV6INIT="${IFCFG_IPV6INIT:-"no"}"
+IFCFG_IPV6_AUTOCONF="${IFCFG_IPV6_AUTOCONF:-"no"}"
+IFCFG_ONBOOT="${IFCFG_ONBOOT:-"yes"}"
+IFCFG_IPADDR="${IFCFG_IPADDR:-"$(get_data "IPADDR")"}"
+IFCFG_PREFIX="${IFCFG_PREFIX:-"8"}"
+IFCFG_GATEWAY="${IFCFG_GATEWAY:-"$(get_data "GATEWAY")"}"
+IFCFG_DNS1="${IFCFG_DNS1:-"1.1.1.1"}"
+IFCFG_DNS2="${IFCFG_DNS2:-"8.8.8.8"}"
 
 
 # ================= DO NOT EDIT BEYOND THIS LINE =================
-
-# get connection values
-bootproto="none"
-ipv6init="no"
-ipv6_autoconf="no"
-onboot="yes"
-ipaddr=$(get_data "IPADDR")
-prefix="8"
-gateway=$(get_data "GATEWAY")
-dns1="1.1.1.1"
-dns2="8.8.8.8"
 
 # get sudo password
 echo "Enter sudo password:"
@@ -36,15 +34,15 @@ run_with_sudo nmcli connection up "${IFCFG_INTERFACE}"
 run_with_sudo cp -f "${IFCFG_CONFIG}" "${IFCFG_CONFIG}.bak"
 
 # update connection config
-run_with_sudo update_config "${IFCFG_CONFIG}" "BOOTPROTO" \"${bootproto}\"
-run_with_sudo update_config "${IFCFG_CONFIG}" "IPV6INIT" \"${ipv6init}\"
-run_with_sudo update_config "${IFCFG_CONFIG}" "IPV6_AUTOCONF" \"${ipv6_autoconf}\"
-run_with_sudo update_config "${IFCFG_CONFIG}" "ONBOOT" \"${onboot}\"
-run_with_sudo update_config "${IFCFG_CONFIG}" "IPADDR" \"${ipaddr}\"
-run_with_sudo update_config "${IFCFG_CONFIG}" "PREFIX" \"${prefix}\"
-run_with_sudo update_config "${IFCFG_CONFIG}" "GATEWAY" \"${gateway}\"
-run_with_sudo update_config "${IFCFG_CONFIG}" "DNS1" \"${dns1}\"
-run_with_sudo update_config "${IFCFG_CONFIG}" "DNS2" \"${dns2}\"
+run_with_sudo update_config "${IFCFG_CONFIG}" "BOOTPROTO" \"${IFCFG_BOOTPROTO}\"
+run_with_sudo update_config "${IFCFG_CONFIG}" "IPV6INIT" \"${IFCFG_IPV6INIT}\"
+run_with_sudo update_config "${IFCFG_CONFIG}" "IPV6_AUTOCONF" \"${IFCFG_IPV6_AUTOCONF}\"
+run_with_sudo update_config "${IFCFG_CONFIG}" "ONBOOT" \"${IFCFG_ONBOOT}\"
+run_with_sudo update_config "${IFCFG_CONFIG}" "IPADDR" \"${IFCFG_IPADDR}\"
+run_with_sudo update_config "${IFCFG_CONFIG}" "PREFIX" \"${IFCFG_PREFIX}\"
+run_with_sudo update_config "${IFCFG_CONFIG}" "GATEWAY" \"${IFCFG_GATEWAY}\"
+run_with_sudo update_config "${IFCFG_CONFIG}" "DNS1" \"${IFCFG_DNS1}\"
+run_with_sudo update_config "${IFCFG_CONFIG}" "DNS2" \"${IFCFG_DNS2}\"
 
 # restart network
 run_with_sudo systemctl restart NetworkManager
