aa80c5fa23293501ec60c0b22cca83e855be4fe2
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-07-06 02:52:04 +0800

    Add helper script for cluster nodes DNS resolution
    Helps meet hostname resolution prerequisites

---
+++ helpers/hostname-resolution.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# get script source
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+SCRIPT_PATH="${SOURCE_DIR}/../scripts"
+
+# source project files
+source "${SCRIPT_PATH}/utils.sh"
+
+# variables
+SERVICE_USER="${SERVICE_USER:-"$(get_data "service user account")"}"
+export SUDO_PASSWD="${SUDO_PASSWD:-"$(get_password "sudo password")"}"
+SSH_PORT="${SSH_PORT:-"22"}"
+
+
+# ================= DO NOT EDIT BEYOND THIS LINE =================
+
+# get IP-hostname pairs of all master nodes
+declare -A master_dns_map
+get_kv_pairs master_dns_map "IP of master node"
+# sort master node keys
+master_keys=($(echo "${!master_dns_map[@]}" | tr " " "\n" | sort))
+
+# get IP-hostname pairs of all worker nodes
+declare -A worker_dns_map
+get_kv_pairs worker_dns_map "IP of worker node"
+# sort worker node keys
+worker_keys=($(echo "${!worker_dns_map[@]}" | tr " " "\n" | sort))
+
+# iterate through worker nodes
+for ip in "${worker_keys[@]}"; do
+    hostname="${worker_dns_map[${ip}]}"
+    hosts_file="${hostname}-hosts.tmp"
+
+    echo "Updating worker: ${hostname} (${ip})"
+
+    # download hosts file
+    ssh "${SERVICE_USER}@${hostname}" -p "${SSH_PORT}" "echo \"${SUDO_PASSWD}\" | sudo -S cat \"/etc/hosts\"" > "${hosts_file}"
+    # modify hosts file
+    update_hosts "${master_keys[0]}" "${master_dns_map[${master_keys[0]}]}" "${hosts_file}"
+    # update hosts file on node
+    ssh "${SERVICE_USER}@${hostname}" -p "${SSH_PORT}" "echo \"${SUDO_PASSWD}\" | sudo -S echo \"$(cat ${hosts_file})\" > \"/etc/hosts\""
+    # remove temporary hosts file
+    rm "${hosts_file}"
+done
+
+# iterate through master nodes
+for ip in "${master_keys[@]}"; do
+    hostname="${master_dns_map[${ip}]}"
+    hosts_file="${hostname}-hosts.tmp"
+
+    echo "Updating master: ${hostname} (${ip})"
+
+    # download hosts file
+    ssh "${SERVICE_USER}@${hostname}" -p "${SSH_PORT}" "echo \"${SUDO_PASSWD}\" | sudo -S cat \"/etc/hosts\"" > "${hosts_file}"
+    # modify hosts file
+    for i in "${master_keys[@]}"; do
+        h="${master_dns_map[${i}]}"
+        update_hosts "${i}" "${h}" "${hosts_file}"
+    done
+    # update hosts file on node
+    ssh "${SERVICE_USER}@${hostname}" -p "${SSH_PORT}" "echo \"${SUDO_PASSWD}\" | sudo -S echo \"$(cat ${hosts_file})\" > \"/etc/hosts\""
+    # remove temporary hosts file
+    rm "${hosts_file}"
+done
