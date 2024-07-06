45b053bc1b9d33e2511e2edae7492558d9b2840f
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-07-06 12:04:58 +0800

    Use variable

--- helpers/update-connection.sh
+++ helpers/update-connection.sh
@@ -20,6 +20,7 @@ IFCFG_PREFIX="${IFCFG_PREFIX:-"8"}"
 IFCFG_GATEWAY="${IFCFG_GATEWAY:-"$(get_data "GATEWAY")"}"
 IFCFG_DNS1="${IFCFG_DNS1:-"1.1.1.1"}"
 IFCFG_DNS2="${IFCFG_DNS2:-"8.8.8.8"}"
+IFCFG_TMP_CONFIG="${IFCFG_TMP_CONFIG:-"${HOME}/$(basename "${IFCFG_CONFIG}").tmp"}"
 
 
 # ================= DO NOT EDIT BEYOND THIS LINE =================
@@ -29,21 +30,21 @@ run_with_sudo nmcli connection up "${IFCFG_INTERFACE}"
 
 # backup connection config
 run_with_sudo cp -f "${IFCFG_CONFIG}" "${IFCFG_CONFIG}.bak"
-run_with_sudo cp -f "${IFCFG_CONFIG}" "${HOME}/$(basename "${IFCFG_CONFIG}").tmp"
+run_with_sudo cp -f "${IFCFG_CONFIG}" "${IFCFG_TMP_CONFIG}"
 
 # update connection config
-update_config "${HOME}/$(basename "${IFCFG_CONFIG}").tmp" "BOOTPROTO" \"${IFCFG_BOOTPROTO}\"
-update_config "${HOME}/$(basename "${IFCFG_CONFIG}").tmp" "IPV6INIT" \"${IFCFG_IPV6INIT}\"
-update_config "${HOME}/$(basename "${IFCFG_CONFIG}").tmp" "IPV6_AUTOCONF" \"${IFCFG_IPV6_AUTOCONF}\"
-update_config "${HOME}/$(basename "${IFCFG_CONFIG}").tmp" "ONBOOT" \"${IFCFG_ONBOOT}\"
-update_config "${HOME}/$(basename "${IFCFG_CONFIG}").tmp" "IPADDR" \"${IFCFG_IPADDR}\"
-update_config "${HOME}/$(basename "${IFCFG_CONFIG}").tmp" "PREFIX" \"${IFCFG_PREFIX}\"
-update_config "${HOME}/$(basename "${IFCFG_CONFIG}").tmp" "GATEWAY" \"${IFCFG_GATEWAY}\"
-update_config "${HOME}/$(basename "${IFCFG_CONFIG}").tmp" "DNS1" \"${IFCFG_DNS1}\"
-update_config "${HOME}/$(basename "${IFCFG_CONFIG}").tmp" "DNS2" \"${IFCFG_DNS2}\"
+update_config "${IFCFG_TMP_CONFIG}" "BOOTPROTO" \"${IFCFG_BOOTPROTO}\"
+update_config "${IFCFG_TMP_CONFIG}" "IPV6INIT" \"${IFCFG_IPV6INIT}\"
+update_config "${IFCFG_TMP_CONFIG}" "IPV6_AUTOCONF" \"${IFCFG_IPV6_AUTOCONF}\"
+update_config "${IFCFG_TMP_CONFIG}" "ONBOOT" \"${IFCFG_ONBOOT}\"
+update_config "${IFCFG_TMP_CONFIG}" "IPADDR" \"${IFCFG_IPADDR}\"
+update_config "${IFCFG_TMP_CONFIG}" "PREFIX" \"${IFCFG_PREFIX}\"
+update_config "${IFCFG_TMP_CONFIG}" "GATEWAY" \"${IFCFG_GATEWAY}\"
+update_config "${IFCFG_TMP_CONFIG}" "DNS1" \"${IFCFG_DNS1}\"
+update_config "${IFCFG_TMP_CONFIG}" "DNS2" \"${IFCFG_DNS2}\"
 
 # overwrite connection config
-run_with_sudo mv -f "${HOME}/$(basename "${IFCFG_CONFIG}").tmp" "${IFCFG_CONFIG}"
+run_with_sudo mv -f "${IFCFG_TMP_CONFIG}" "${IFCFG_CONFIG}"
 
 # restart network
 run_with_sudo systemctl restart NetworkManager
