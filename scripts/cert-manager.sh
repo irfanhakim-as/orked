3a71b049d589f352690801677c362d4c2017d651
Irfan Hakim <irfanhakim.as@yahoo.com>
2025-06-01 10:02:14 +0800

    Upgrade cert-manager from v1.11.0 to v1.16.5

--- scripts/cert-manager.sh
+++ scripts/cert-manager.sh
@@ -46,7 +46,7 @@ helm repo update jetstack
 helm upgrade --install cert-manager jetstack/cert-manager \
 --namespace cert-manager \
 --create-namespace \
---version v1.11.0 \
+--version v1.16.5 \
 --set installCRDs=true \
 --set podDnsConfig.options[0].name=ndots \
 --set-literal podDnsConfig.options[0].value=1 \
